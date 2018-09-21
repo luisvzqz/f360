67a3da00fca3c11a96c2e53d0438a53e53c7df7e
Luis Vazquez <luis.vzqz@LuisVzqz.local>
2018-09-20 20:47:09 -0500

    Corrected 360fin_lista_negra

--- 360fin/models/f360_lista_negra.py
+++ 360fin/models/f360_lista_negra.py
@@ -5,10 +5,9 @@ from odoo import api, fields, models
 import datetime
 
 class ListaNegra(models.Model):
-    _inherit = "x_360fin.lista_negra"
+    _name = "x_360fin.lista_negra"
+    _description = "360fin.lista_negra"
 
-
-
-
-
-
+    x_name = fields.Char('Nombre', required=True, index=True)
+    x_company_id = fields.Many2one(comodel_name="res.company", string="Compañía", required=False, help="Compañía")
+    x_partner_id = fields.Many2one(comodel_name="res.partner", string="Partner", required=False, help="Asociado/Cliente")
