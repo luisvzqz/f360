0daa720abdd47843281ca6d651fb95b7d265a81b
Luis Vazquez <luis.vzqz@LuisVzqz.local>
2018-09-20 21:06:49 -0500

    Corrected x_employee_id

--- 360fin/models/f360_lista_negra.py
+++ 360fin/models/f360_lista_negra.py
@@ -11,3 +11,5 @@ class ListaNegra(models.Model):
     x_name = fields.Char('Nombre', required=True, index=True)
     x_company_id = fields.Many2one(comodel_name="res.company", string="Compañía", required=False, help="Compañía")
     x_partner_id = fields.Many2one(comodel_name="res.partner", string="Partner", required=False, help="Asociado/Cliente")
+    x_employee_id = fields.Many2one(comodel_name="hr.employee", string="Empleado", required=False, help="Empleado")
+    x_user_id = fields.Many2one(comodel_name="res.users", string="Usuario", required=False, help="Usuario")
