ffd28f2da4cd8519be637ae2fbf48536f2085bf6
AnibalArenasP <anibal@gmc360.com.mx>
2018-10-08 19:21:37 -0500

    amortiza.py

--- 360fin/models/f360_amortiza.py
+++ 360fin/models/f360_amortiza.py
@@ -18,18 +18,23 @@ class F360Amortiza(models.Model):
     _name = "x_360fin.amortiza"
     _description = "360fin.amortiza"
 
-    x_balance = fields.Float(compute='_compute_x_balance', required=True, index=True, store=True)
-    x_balance_tax = fields.Float(compute='_compute_x_balance_tax', required=True, index=True, store=True)
-    x_capital = fields.Float(compute='_compute_x_capital', required=True, index=True, store=True)
-    x_company_id = fields.Many2one(comodel_name="res.company", string="Código company id", required=False,
-                                   help="Company id")
-    x_date = fields.Datetime('Date')
-    x_loan_fin = fields.Float(compute='_compute_x_loan_fin', required=True, index=True, store=True)
-    x_loan_ini = fields.Float(compute='_compute_x_loan_ini', required=True, index=True, store=True)
+    create_date = fields.datetime(string='Created on', store=True, copy=True )
+    create_uid = fields.many2one(string='Created by',store=True, copy=True, comodel_name="res.users")
+    display_name = fields.Char(string='Display Name', readonly=True) #falta "tamaño"
+    id = fields.Integer(string='ID', readonly=True, store=True, copy=True)
+    write_date = fields.datetime(string='Last Updated on', store=True, copy=True)
+    write_uid = fields.many2one(string='Last Updated by', store=True, copy=True, comodel_name="res.users")
+
+    x_balance = fields.Float(string='Balance', store=True, copy=True, help="Balance")
+    x_balance_tax = fields.Float(string='Balance with tax', store=True, copy=True)
+    x_capital = fields.Float(string='Payments to Capital', store=True, copy=True)
+    x_company_id = fields.Many2one(string='Company id', store=True, copy=True, help="Company id", comodel_name="res.company")
+    x_date = fields.Datetime(string='Fecha', store=True, copy=True, help="Fecha")
+    x_loan_fin = fields.Float(string='Loan Final', store=True, copy=True, help="Loan Final")
+    x_loan_ini = fields.Float(string='Loan Initial', store=True, copy=True, help="Loan Initial")
     x_n = fields.Integer('Number of Payment', default=1, help="Number of payment")
     x_name = fields.Char('Amortiza', required=True, index=True)
-    x_opportunity_id = fields.Many2one(comodel_name="crm.lead", string="Código opportunity id", required=False,
-                                   help="Opportunity id")
+    x_opportunity_id = fields.Many2one(comodel_name="crm.lead", string="Código opportunity id", required=False, help="Opportunity id")
     x_order_id = fields.Many2one(comodel_name="sale.order", string="Código order id", required=False,
                                        help="Order id")
     x_partner_id = fields.Many2one(comodel_name="res.partner", string="Código partner id", required=False,
